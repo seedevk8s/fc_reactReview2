400277d35b6d8d916055817a58e44b03825205a0
seedevk8s <seedevk8s@gmail.com>
2020-04-06 18:02:18 +0900

    버튼 색상 설정하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import Button from "./components/Button";
 
 function App() {
@@ -9,6 +9,24 @@ function App() {
         <Button size="large">BUTTON</Button>
         <Button>BUTTON</Button>
         <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="gray">
+          BUTTON
+        </Button>
+        <Button color="gray">BUTTON</Button>
+        <Button size="small" color="gray">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button size="small" color="pink">
+          BUTTON
+        </Button>
       </div>
     </div>
   );
