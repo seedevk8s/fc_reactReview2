bc0a65caf86a2ce81751806e6b8fe43a06e65542
seedevk8s <seedevk8s@gmail.com>
2020-04-06 16:35:00 +0900

    버튼 사이즈 조정하기

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Button.scss";
+import classNames from "classnames";
 
-function Button({ children }) {
-  return <button className="Button">{children}</button>;
+function Button({ children, size }) {
+  /* className 에 CSS 클래스 이름을 동적으로 넣어주고 싶으면 이렇게 설정을 해주어야 합니다. */
+  /* props 로 받은 props 값이 button 태그의 className 으로 전달이 될 것 입니다 */
+  return <button className={classNames("Button", size)}>{children}</button>;
 }
+
+Button.defaultProps = {
+  size: "medium",
+};
 
 export default Button;
