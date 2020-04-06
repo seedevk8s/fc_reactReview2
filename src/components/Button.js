ae0863fd546eec8f281554c6f23e49a6494b071d
seedevk8s <seedevk8s@gmail.com>
2020-04-06 16:54:17 +0900

    CSS 클래스에 따라 다른 색상이 적용되도록

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./Button.scss";
 import classNames from "classnames";
 
-function Button({ children, size }) {
+function Button({ children, size, color }) {
   /* className 에 CSS 클래스 이름을 동적으로 넣어주고 싶으면 이렇게 설정을 해주어야 합니다. */
   /* props 로 받은 props 값이 button 태그의 className 으로 전달이 될 것 입니다 */
-  return <button className={classNames("Button", size)}>{children}</button>;
+  return (
+    <button className={classNames("Button", size, color)}>{children}</button>
+  );
 }
 
 Button.defaultProps = {
   size: "medium",
+  color: "blue",
 };
 
 export default Button;
