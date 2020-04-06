dbd568cc501da765498bee53a85b4fd63b05dcd4
seedevk8s <seedevk8s@gmail.com>
2020-04-06 21:16:01 +0900

    ...rest props 전달하기-1

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./Button.scss";
 import classNames from "classnames";
 
-function Button({ children, size, color, outline, fullWidth }) {
+/* 필요한 이벤트가 있을 때 마다 매번 이렇게 넣어주는건 귀찮습니다. 
+이러한 문제를 해결 해줄 수 있는 문법이 있는데요! 바로 spread 와 rest 입니다.  */
+/* 이 문법은 주로 배열과 객체, 함수의 파라미터, 인자를 다룰 때 사용하는데, 
+컴포넌트에서도 사용 할 수 있답니다. */
+function Button({
+  children,
+  size,
+  color,
+  outline,
+  fullWidth,
+  onClick,
+  onMouseMove,
+}) {
   /* className 에 CSS 클래스 이름을 동적으로 넣어주고 싶으면 이렇게 설정을 해주어야 합니다. */
   /* props 로 받은 props 값이 button 태그의 className 으로 전달이 될 것 입니다 */
   /* outline 값을 props 로 받아와서 객체 안에 집어 넣은 다음에 classNames() 에 포함시켜줬는데 
@@ -10,6 +22,8 @@ function Button({ children, size, color, outline, fullWidth }) {
   return (
     <button
       className={classNames("Button", size, color, { outline, fullWidth })}
+      onClick={onClick}
+      onMouseMove={onMouseMove}
     >
       {children}
     </button>
